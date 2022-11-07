51ef22c44faf1ebbeb64cd1745278973c1f781d5
Maria Sadovoi <marysadovoi@gmail.com>
2022-11-07 13:11:25 +0200

    files commit

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser 
+    {
+        [Key]
+        public string Id { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Race { get; set; }
+    }
+}
